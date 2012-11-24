48d1744d08232088d2be0792de7139d4fecbe49b
Martin <Martin@Martin>
2012-11-24 22:51:08 +0100

    table model

--- DPO-4/src/cz/pelantciz/dpo4/Window.java
+++ DPO-4/src/cz/pelantciz/dpo4/Window.java
@@ -7,6 +7,8 @@ import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -22,7 +24,7 @@ import cz.pelantciz.dpo4.shapes.Shape;
 import cz.pelantciz.dpo4.shapes.Square;
 import cz.pelantciz.dpo4.ui.MyCanvas;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements MouseListener {
     public static final String TAG = "Window";
 
     private List<Shape> shapes = new ArrayList<Shape>();
@@ -56,57 +58,27 @@ public class Window extends JFrame {
         getContentPane().add(canvas);
         getContentPane().add(prepareRightPanel());
 
-       
     }
 
     private void invalidateViews() {
         canvas.update(canvas.getGraphics());
         System.out.println("invalidated");
     }
-    
+
     private Canvas prepareCanvas() {
         MyCanvas canvas = new MyCanvas();
         canvas.setShapes(shapes);
-        canvas.addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseReleased(MouseEvent event) {
-                if (event.getButton() == event.BUTTON1) {
-                    shapes.add(new Circle(event.getX(), event.getY()));
-                    invalidateViews();
-                }else if(event.getButton() == event.BUTTON3) {
-                    shapes.add(new Square(event.getX(), event.getY()));
-                    invalidateViews();
-                }
-            }
-
-            @Override
-            public void mousePressed(MouseEvent arg0) {
-            }
-
-            @Override
-            public void mouseExited(MouseEvent arg0) {
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent arg0) {
-            }
-
-            @Override
-            public void mouseClicked(MouseEvent event) {
-
-            }
-        });
+        canvas.addMouseListener(this);
         return canvas;
     }
-    
+
     private JPanel prepareRightPanel() {
         JPanel panel = new JPanel();
         panel.setBackground(Color.WHITE);
         getContentPane().add(panel);
         panel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        
+
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
         c.weighty = 1;
@@ -121,12 +93,51 @@ public class Window extends JFrame {
         table.setBackground(Color.YELLOW);
         c.gridy = 1;
         panel.add(table, c);
-        
+
         JButton button = new JButton("Clear");
+        button.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                shapes.clear();
+                invalidateViews();
+            }
+        });
         c.weighty = 0;
         c.gridy = 2;
         panel.add(button, c);
-        
+
         return panel;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        if (event.getButton() == event.BUTTON1) {
+            shapes.add(new Circle(event.getX(), event.getY()));
+            invalidateViews();
+        } else if (event.getButton() == event.BUTTON3) {
+            shapes.add(new Square(event.getX(), event.getY()));
+            invalidateViews();
+        }
+
     }
 }
---
+++ DPO-4/src/cz/pelantciz/dpo4/ui/MyTableModel.java
@@ -0,0 +1,36 @@
+package cz.pelantciz.dpo4.ui;
+
+import javax.swing.table.AbstractTableModel;
+
+public class MyTableModel extends AbstractTableModel{
+    public static final String TAG = "MyTableModel";
+
+    
+    
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return true;
+    }
+    
+    
+    
+
+}
